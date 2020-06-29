8fdc630c102b0d6e132583c8e7f1b78b85d6fcc6
joaquim_og@hotmail.com <virgem89>
2020-06-29 20:30:18 -0300

    update project for Kotlin language

--- app/src/main/java/com/joaquim/replicanetflix/kotlin/MainActivity.kt
+++ app/src/main/java/com/joaquim/replicanetflix/kotlin/MainActivity.kt
@@ -1,9 +1,101 @@
 package com.joaquim.replicanetflix.kotlin
 
-import com.joaquim.replicanetflix.MainActivity
+import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.joaquim.replicanetflix.R
+import com.joaquim.replicanetflix.model.Category
+import com.joaquim.replicanetflix.model.Movie
+import com.joaquim.replicanetflix.util.CategoryTask
+import com.joaquim.replicanetflix.util.ImageDownloaderTask
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.category_item.view.*
+import kotlinx.android.synthetic.main.movie_item.view.*
 
-class MainActivity : MainActivity() {
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainAdapter: MainAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val categories = arrayListOf<Category>()
+        mainAdapter = MainAdapter(categories)
+        recycler_view_main.adapter = mainAdapter
+        recycler_view_main.layoutManager = LinearLayoutManager(this)
+
+        val categoryTask = CategoryTask(this)
+        categoryTask.setCategoryLoader { categories ->
+            mainAdapter.categories.clear()
+            mainAdapter.categories.addAll(categories)
+            mainAdapter.notifyDataSetChanged()
+        }
+
+        categoryTask.execute("https://tiagoaguiar.co/api/netflix/home")
+    }
+
+    private inner class MainAdapter(val categories: MutableList<Category>) : RecyclerView.Adapter<CategoryHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryHolder {
+            val categoryHolder = CategoryHolder(
+                    layoutInflater.inflate(R.layout.category_item, parent, false)
+            )
+
+            return categoryHolder
+        }
+
+        override fun getItemCount(): Int {
+            return categories.size
+        }
+
+        override fun onBindViewHolder(holder: CategoryHolder, position: Int) {
+            val category = categories[position]
+            holder.bind(category)
+        }
+    }
 
 
+
+    private inner class MovieAdapter(val movies: MutableList<Movie>) : RecyclerView.Adapter<MovieHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieHolder {
+            val movieHolder = MovieHolder(
+                    layoutInflater.inflate(R.layout.category_item, parent, false)
+            )
+
+            return movieHolder
+        }
+        override fun getItemCount(): Int {
+            return movies.size
+        }
+
+        override fun onBindViewHolder(holder: MovieHolder, position: Int) {
+            val movie = movies[position]
+            holder.bind(movie)
+        }
+
+    }
+
+    private inner class CategoryHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(category: Category) {
+            itemView.text_view_title.text = category.name
+            itemView.recycler_view_movie.adapter = MovieAdapter(category.movies)
+            itemView.recycler_view_movie.layoutManager = LinearLayoutManager(this@MainActivity, RecyclerView.HORIZONTAL, false)
+        }
+    }
+
+    private class MovieHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(movie: Movie) {
+            ImageDownloaderTask(itemView.image_view_cover)
+                    .execute(movie.coverUrl)
+
+//            itemView.image_view_cover.setOnClickListener{
+//
+//            }
+        }
+    }
 
 }
